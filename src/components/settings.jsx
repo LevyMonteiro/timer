845feb5d38ce1f098b4a5143f4c737522570accc
Levy Monteiro <levymonteiro268@gmail.com>
2023-10-06 16:42:31 -0300

    update Settings component increment and decrement icons

--- src/components/settings.jsx
+++ src/components/settings.jsx
@@ -2,16 +2,16 @@ const Settings = () => (
   <>
     <label id='break-label'>
       <p>Break Length</p>
+      <button id='break-decrement'><i class="fa-solid fa-arrow-down"></i></button>
       <p id="break-length">5</p>
-      <button id='break-decrement'></button>
-      <button id='break-increment'></button>
+      <button id='break-increment'><i class="fa-solid fa-arrow-up"></i></button>
     </label>
 
     <label id='session-label'>
       <p>Session Length</p>
+      <button id='session-decrement'><i class="fa-solid fa-arrow-down"></i></button>
       <p id="session-length">25</p>
-      <button id='session-decrement'></button>
-      <button id='session-increment'></button>
+      <button id='session-increment'><i class="fa-solid fa-arrow-up"></i></button>
     </label>
   </>
 )
