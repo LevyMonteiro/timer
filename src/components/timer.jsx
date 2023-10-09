663a08f9e3ac03e148ce953631eb835108ccfebc
Levy Monteiro <levymonteiro268@gmail.com>
2023-10-09 18:11:01 -0300

    create timeFormatter function

--- src/components/timer.jsx
+++ src/components/timer.jsx
@@ -1,14 +1,27 @@
-const Timer = () => (
- <>
-  <label id="timer-label">
-    <h2>Session</h2>
-    <div id="time-left">
-      <span id="session-minutes">25</span>:<span id="session-seconds">00</span>
-    </div>
-    <button id="start_stop"><i className="fa-solid fa-play"></i></button>
-    <button id="reset"><i className="fa-solid fa-rotate"></i></button>
-  </label>
- </>
-)
+import { useState } from 'react'
+
+const Timer = () => {
+  const [timeLeft, setTimeLeft] = useState(1500)
+
+  function timeFormatter() {
+    const minutes = Math.floor(timeLeft / 60)
+    const seconds = timeLeft - minutes * 60
+    const formattedMinutes = minutes < 10 ? '0' + minutes : minutes
+    const formattedSeconds = seconds < 10 ? '0' + seconds : seconds
+
+    return `${formattedMinutes}:${formattedSeconds}`
+  }
+
+  return (
+    <>
+      <label id="timer-label">
+        <h2>Session</h2>
+        <div id="time-left">{ timeFormatter() }</div>
+        <button id="start_stop"><i className="fa-solid fa-play"></i></button>
+        <button id="reset"><i className="fa-solid fa-rotate"></i></button>
+      </label>
+    </>
+  ) 
+}
 
 export { Timer }
