786e2622697acbda7bf73ce78fa49a02b982229d
Levy Monteiro <levymonteiro268@gmail.com>
2023-10-09 15:13:13 -0300

    create Settings handleBreak increase && decrease functions

--- src/components/settings.jsx
+++ src/components/settings.jsx
@@ -1,19 +1,37 @@
-const Settings = () => (
-  <>
-    <label id='break-label'>
-      <p>Break Length</p>
-      <button id='break-decrement'><i class="fa-solid fa-arrow-down"></i></button>
-      <p id="break-length">5</p>
-      <button id='break-increment'><i class="fa-solid fa-arrow-up"></i></button>
-    </label>
+import  { useState } from 'react'
 
-    <label id='session-label'>
-      <p>Session Length</p>
-      <button id='session-decrement'><i class="fa-solid fa-arrow-down"></i></button>
-      <p id="session-length">25</p>
-      <button id='session-increment'><i class="fa-solid fa-arrow-up"></i></button>
-    </label>
-  </>
-)
+const Settings = () => {  
+  const [breakLength, setBreakLength] = useState(5)
+
+  function handleBreakDecrease() {
+    if (breakLength > 1) {
+      setBreakLength(breakLength - 1)
+    }
+  }
+
+  function handleBreakIncrease() {
+    if (breakLength < 60) {
+      setBreakLength(breakLength + 1)
+    }
+  }
+  
+  return  (
+    <>
+      <label id='break-label'>
+        <p>Break Length</p>
+        <button id='break-decrement' onClick={handleBreakDecrease}><i class="fa-solid fa-arrow-down"></i></button>
+        <p id="break-length">{ breakLength }</p>
+        <button id='break-increment' onClick={handleBreakIncrease}><i class="fa-solid fa-arrow-up"></i></button>
+      </label>
+
+      <label id='session-label'>
+        <p>Session Length</p>
+        <button id='session-decrement'><i class="fa-solid fa-arrow-down"></i></button>
+        <p id="session-length">25</p>
+        <button id='session-increment'><i class="fa-solid fa-arrow-up"></i></button>
+      </label>
+    </>
+  )
+}
 
 export { Settings }
