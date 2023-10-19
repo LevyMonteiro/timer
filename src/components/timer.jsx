3f45ac11eabdd7eb82672bab96f1311f035231b9
Levy Monteiro <levymonteiro268@gmail.com>
2023-10-19 17:11:10 -0300

    update butons to div.btn

--- src/components/timer.jsx
+++ src/components/timer.jsx
@@ -110,23 +110,23 @@ const Timer = () => {
       <h1>Timer</h1>
       <div id='break-label'>
         <p>Break Length</p>
-        <button id='break-decrement' onClick={handleBreakDecrease} disabled={play}><i className="fa-solid fa-arrow-down"></i></button>
+        <div className='btn' id='break-decrement' onClick={handleBreakDecrease} disabled={play}><i className="fa-solid fa-arrow-down"></i></div>
         <p id="break-length">{breakLength}</p>
-        <button id='break-increment' onClick={handleBreakIncrease} disabled={play}><i className="fa-solid fa-arrow-up"></i></button>
+        <div className='btn' id='break-increment' onClick={handleBreakIncrease} disabled={play}><i className="fa-solid fa-arrow-up"></i></div>
       </div>
 
       <div id='session-label'>
         <p>Session Length</p>
-        <button id='session-decrement' onClick={handleSessionDecrease} disabled={play}><i className="fa-solid fa-arrow-down"></i></button>
+        <div className='btn' id='session-decrement' onClick={handleSessionDecrease} disabled={play}><i className="fa-solid fa-arrow-down"></i></div>
         <p id="session-length">{sessionLength}</p>
-        <button id='session-increment' onClick={handleSessionIncrease} disabled={play}><i className="fa-solid fa-arrow-up"></i></button>
+        <div className='btn' id='session-increment' onClick={handleSessionIncrease} disabled={play}><i className="fa-solid fa-arrow-up"></i></div>
       </div>
 
       <div id="timer-label">
         <h2>{timingType}</h2>
         <div id="time-left">{timeFormatter()}</div>
-        <button id="start_stop" onClick={handlePlay}><i className={!play ? "fa-solid fa-play" : "fa-solid fa-pause" }></i></button>
-        <button id="reset" onClick={handleReset}><i className="fa-solid fa-rotate"></i></button>
+        <div className='btn' id="start_stop" onClick={handlePlay}><i className={!play ? "fa-solid fa-play" : "fa-solid fa-pause" }></i></div>
+        <div className='btn' id="reset" onClick={handleReset}><i className="fa-solid fa-rotate"></i></div>
       </div>
 
       <audio
